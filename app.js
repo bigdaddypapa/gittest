423e240982afb6b6e86d3c980e9245cbbed60ee6
unknown <pape2006ndoye@gmail.com>
2023-03-01 15:42:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Variables
+           //var, const, let
+var cat;
+cat = "Fido"
+console.log  (cat)
+
+// Data Types
+//Conditionals
+//Arrays
+//Loops
+//Object
+// let grade = 55
+// if (grade >= 90){
+// console.log "A"}
+// if (grade >= 80){
+// console.log "B"}
+// if (grade >=70){
+// console.log "C"}
+// if (grade >=55){
+// console.log "C"}
+// else
+// {
+// console.log  "F"
+// }
+for(let num=1; num<=10; num+2 ){
+    console.log(num);
+}
+for( let i=)
